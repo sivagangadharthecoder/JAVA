4300e9d47de35b52d06abcd1051b3aa411a8948d
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-01 11:15:44 +0530

    Add files via upload

---
+++ CODECHEF.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner obj = new Scanner(System.in);
+        
+        int a = obj.nextInt();
+        if(a == 4)
+        {
+            System.out.print("YES");
+        }
+        else{
+            System.out.print("NO");
+        }
+	}
+}
