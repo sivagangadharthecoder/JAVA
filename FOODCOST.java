7588f6c049121c05a7b588c0f4df5bbd3230435f
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-01 11:56:47 +0530

    Add files via upload

---
+++ FOODCOST.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner obj = new Scanner(System.in);
+        
+        int x = obj.nextInt();
+        int y = obj.nextInt();
+        
+        System.out.print(6*x + y);
+	}
+}
